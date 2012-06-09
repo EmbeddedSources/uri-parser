68a3499ae3808fccdc36d5889b30e88771176d7c
dodikk <dodikk88.reg@gmail.com>
2012-06-09 18:53:31 +0300

    Improved example readability

--- single-example.cpp
+++ single-example.cpp
@@ -7,12 +7,12 @@ int main(int argc, const char *argv[])
     if (argc < 2) return 1;
     std::string haystack = argv[1];
     http::url parsed = http::ParseHttpUrl(haystack);
-    std::cout << parsed.protocol << " "
-              << parsed.user << " "
-              << parsed.password << " "
-              << parsed.host << " "
-              << parsed.port << " "
-              << parsed.path << " "
-              << parsed.search << std::endl;
+    std::cout << "1 " << parsed.protocol << "\n"
+              << "2 " << parsed.user << "\n"
+              << "3 " << parsed.password << "\n"
+              << "4 " << parsed.host << "\n"
+              << "5 " << parsed.port << "\n"
+              << "6 " << parsed.path << "\n"
+              << "7 " << parsed.search << std::endl;
     return 0;
 }
